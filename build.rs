a8a64ba2ca48dd0b0de10859b76044c4751abf64
Vhyrro <vhyrro@gmail.com>
2023-03-11 14:58:10 +0100

    ref: generate `bindings.h` in the `OUT_DIR` directory

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 extern crate cbindgen;
-use std::env;
+use std::{env, path::Path};
 
 pub fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings!")
-        .write_to_file("src/bindings.h");
+        .write_to_file(Path::new(&env::var("OUT_DIR").unwrap()).join("bindings.h"));
 }
